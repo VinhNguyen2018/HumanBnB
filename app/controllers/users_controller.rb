ec9e805a547a680e9e830eb5e978ea72694c857c
Francois Nguyen <francois.nguyen2@gmail.com>
2019-02-19 22:19:03 +0100

    comment on current_user in services and deleted test user_signed_in in homepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'Bienvenue sur HumanBnB !'
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  def dashboard
+    @User = current_user
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.delete
+    redirect_to root_path
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
