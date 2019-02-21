35bac9938c7a469949f65bfe6d3346e145baf9d1
Francois Nguyen <francois.nguyen2@gmail.com>
2019-02-21 17:25:32 +0100

    finished js on dashboard

--- app/javascript/components/tabs.js
+++ app/javascript/components/tabs.js
@@ -1,19 +1,14 @@
-
-// console.log("Hello from app/javascript/packs/application.js!");
-
 const initToggleActiveTabs = () => {
-  const tabs = document.querySelectorAll('.tabs-underlined .tab-underlined');
-  const vente = document.getElementById('vente active');
+  const tabs = document.querySelectorAll('.tab-underlined');
+  const vente = document.getElementById('vente');
   const achat = document.getElementById('achat');
   tabs.forEach( (tab) => {
     tab.addEventListener("click", (event) => {
-      // const test = event.currentTarget;
-      console.log(test);
-      console.log(event);
-      event.currentTarget.classList.toggle("tab-active");
-      // event.classList.toggle("active");
-      // achat.classList.toggle("active");
-      // vente.classList.toggle("active");
+      tabs.forEach((click)=> {
+        click.classList.toggle("tab-active");
+      })
+      vente.classList.toggle("hidden");
+      achat.classList.toggle("active");
     })
   })
 }
